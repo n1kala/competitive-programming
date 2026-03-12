b30f29550a5f34a4d1f422f1322934d6bb37e2f8
n1kala <nkala24@freeuni.edu.ge>
2026-03-12 19:25:13 +0400

    initial commit

--- Caesar's Legions.cpp
+++ Caesar's Legions.cpp
@@ -2,8 +2,47 @@
 using namespace std;
 #define MOD 100000000
 
+// total footmen, total horsemen, consecutive footmen in the end, same on horsmen
+int dp[101][101][11][11];
+
 int main() {
     int n1,n2,k1,k2;
     cin >> n1 >> n2 >> k1 >> k2;
-    int
+    // absolute base
+    for(int i = 1; i <= 10; i++) dp[i][0][i][0] = 1;
+    for(int i = 1; i <= 10; i++) dp[0][i][0][i] = 1;
+
+    for(int i = 1; i <= 100; i++) {
+        for(int j = 1; j <= 100; j++) {
+            // if we have consecutive same type soldiers of amount i1
+            // we must have added it next to i1-1 consecutive soldiers
+            // or as a new type of soldier next to some j1 amount of others
+            for(int i1 = 1; i1 <= k1; i1++) {
+                dp[i][j][i1][0] = dp[i-1][j][i1-1][0];
+                for(int j1 = 1; j1 <= k2; j1++) {
+                    if(i1 == 1) {
+                        dp[i][j][i1][0] += dp[i-1][j][0][j1];
+                        dp[i][j][i1][0] %= MOD;
+                    }
+                }
+            }
+            for(int i1 = 1; i1 <= k2; i1++) {
+                dp[i][j][0][i1] = dp[i][j-1][0][i1-1];
+                for(int j1 = 1; j1 <= k1; j1++) {
+                    if(i1 == 1) {
+                        dp[i][j][0][i1] += dp[i][j-1][j1][0];
+                        dp[i][j][0][i1] %= MOD;
+                    }
+                }
+            }
+        }
+    }
+    long long ans = 0;
+    for(int i1 = 1; i1 <= k1; i1++) {
+        ans += dp[n1][n2][i1][0];
+    }
+    for(int i1 = 1; i1 <= k2; i1++) {
+        ans += dp[n1][n2][0][i1];
+    }
+    cout << ans%MOD << endl;
 }
--- tree shufling.cpp
+++ tree shufling.cpp
@@ -1,53 +1,141 @@
 #include <bits/stdc++.h>
 using namespace std;
-int arr[200001][3];
-vector<int> v[200001],p(200001);
-int have[200001][2], need[200001][2];
-int ans;
-void dfs(int par, int cur, int mn) {
-    mn = min(mn, arr[cur][0]);
-    arr[cur][0] = mn;
-    if(arr[cur][1] != arr[cur][2]) {
-        have[par][0] += arr[cur][1] == 0;
-        have[par][1] += arr[cur][1] == 1;
 
-        need[par][0] += arr[cur][2] == 0;
-        need[par][1] += arr[cur][2] == 1;
-    }
+vector<vector<int>> children(200001), temp(200001), levels(200001);
+int n;
+int parent[200001];
+int price[200001];
+int level[200001];
+bool number[200001];
+bool desire[200001];
+int ones[200001];
+int onesDesired[200001];
+int zeros[200001];
+int zerosDesired[200001];
+bool been[200001];
+long long answer = 0;
 
-    for(auto &x : v[cur]) {
-        dfs(cur, x, mn);
+bool checkMatch(int cur) {
+    return number[cur] == desire[cur];
+}
+
+void increaseCounts(int cur) {
+    ones[cur] += number[cur] == 1;
+    zeros[cur] += number[cur] == 0;
+    onesDesired[cur] += desire[cur] == 1;
+    zerosDesired[cur] += desire[cur] == 0;
+}
+
+void countOnesZerosAndMatches(int cur) {
+    if(!checkMatch(cur)) {
+        increaseCounts(cur);
     }
 }
 
-void solve(int cur) {
-    int hv0 = have[cur][0], hv1 = have[cur][1];
-    int nd0 = need[cur][0], nd1 = need[cur][1];
-    int a = min(hv0,nd0), b = min(hv1,nd1);
-    ans += (a + b) * arr[cur][0];
-    if(cur == 1) return;
-    have[p[cur]][0] += have[cur][0] - a;
-    have[p[cur]][1] += have[cur][1] - b;
-    need[p[cur]][0] += need[cur][0] - a;
-    need[p[cur]][1] += need[cur][1] - b;
-    solve(p[cur]);
+void matchAndRedirect(int cur) {
+    // basecase
+    if(cur == 1) {
+        return;
+    }
+
+    // we have more zeros than we can handle, so we redirect them to parent
+    if(zeros[cur] > zerosDesired[cur]) {
+        zeros[parent[cur]] += zeros[cur] - zerosDesired[cur];
+        zeros[cur] = zerosDesired[cur];
+    }
+
+    // same but on ones
+    if(ones[cur] > onesDesired[cur]) {
+        ones[parent[cur]] += ones[cur] - onesDesired[cur];
+        ones[cur] = onesDesired[cur];
+    }
+
+    // we also move desires up
+    if(zerosDesired[cur] > zeros[cur]) {
+        zerosDesired[parent[cur]] += zerosDesired[cur] - zeros[cur];
+        zerosDesired[cur] = zeros[cur];
+    }
+
+    if(onesDesired[cur] > ones[cur]) {
+        onesDesired[parent[cur]] += onesDesired[cur] - ones[cur];
+        onesDesired[cur] = ones[cur];
+    }
+}
+
+void countAnswer(int cur) {
+    // now we know exactly how much should we match with current node's price
+    long long currentPrice = price[cur];
+    long long currentOnes = onesDesired[cur], currentZeroes = zerosDesired[cur];
+    long long cost = currentPrice*(currentOnes + currentZeroes);
+    answer += cost;
 }
 
 int main() {
-    int n; cin >> n;
+    cin >> n;
+
+    int cntl = 0, cntr = 0;
     for(int i = 0; i < n; i++) {
-        cin >> arr[i+1][0] >> arr[i+1][1] >> arr[i+1][2];
+        cin >> price[i+1] >> number[i+1] >> desire[i+1];
+        cntl += number[i+1] == 1;
+        cntr += desire[i+1] == 1;
     }
+
+    if(cntl != cntr) {
+        cout << -1 << endl;
+        return 0;
+    }
+
+    // saving edges to remember them go from root to leaves later
     for(int i = 0; i < n-1; i++) {
         int a,b; cin >> a >> b;
-        v[a].push_back(b);
-        p[b] = a;
+        temp[a].push_back(b);
+        temp[b].push_back(a);
     }
-    dfs(0, 1, arr[1][0]);
-    for(int i = 0; i < n; i++) {
-        if(v[i+1].size() == 0) {
-            solve(i+1);
+
+    // if parent is cheaper than child, its logical to redirect every
+    // child to the parent. to do this we just change child's price and
+    // not actually redirect anything to save time
+    queue<int> q; q.push(1);
+    while(!q.empty()) {
+        int cur = q.front(); q.pop();
+        been[cur] = 1;
+        if(!level[cur]) {
+            level[cur] = level[parent[cur]] + 1;
+            levels[level[cur]].push_back(cur);
+        }
+        for(auto &x : temp[cur]) {
+            if(been[x]) continue;
+            been[x] = 1;
+            children[cur].push_back(x);
+            parent[x] = cur;
+            price[x] = min(price[x], price[cur]);
+            q.push(x);
         }
     }
-    cout << ans << endl;
+
+    // now we start from leaves, match all we can in current node and
+    // redirect rest to parent. in short we just change parent of the
+    // ones that we could not match. but this would be too costy, so
+    // instead of changing parents and checking if it matches, we
+    // store total amount of ones and zeros and matches under each
+    // node and then we only redirect numbers and never change actual
+    // parents of any node
+    for(int i = 1; i <= n; i++) {
+        countOnesZerosAndMatches(i);
+    }
+
+    for(int i = n; i > 1; i--) {
+        for(auto &x : levels[i]) {
+            matchAndRedirect(x);
+        }
+    }
+
+    // finally we count answer, since we know how many should we
+    // shuffle for each node and what will it cost
+    for(int i = 1; i <= n; i++) {
+        countAnswer(i);
+    }
+
+    cout << answer << endl;
+
 }
